a568063d49afc1cac890658db1586031ef5e2886
juniuszhou <junius.zhou@gmail.com>
2020-01-28 21:44:59 +0800

    set all data member as pub for data migration.

--- src/lib.rs
+++ src/lib.rs
@@ -214,9 +214,7 @@ use serde_derive::{Deserialize, Serialize};
 use rstd::prelude::*;
 
 use codec::{Decode, Encode};
-use srml_support::{
-    decl_event, decl_module, decl_storage, dispatch, ensure,
-};
+use srml_support::{decl_event, decl_module, decl_storage, dispatch, ensure};
 
 mod mock;
 mod tests;
@@ -331,8 +329,8 @@ pub trait ForumUserRegistry<AccountId> {
 #[cfg_attr(feature = "std", derive(Serialize, Deserialize, Debug))]
 #[derive(Encode, Decode, Default, Clone, PartialEq, Eq)]
 pub struct BlockchainTimestamp<BlockNumber, Moment> {
-    block: BlockNumber,
-    time: Moment,
+    pub block: BlockNumber,
+    pub time: Moment,
 }
 
 /// Represents a moderation outcome applied to a post or a thread.
@@ -340,13 +338,13 @@ pub struct BlockchainTimestamp<BlockNumber, Moment> {
 #[derive(Encode, Decode, Default, Clone, PartialEq, Eq)]
 pub struct ModerationAction<BlockNumber, Moment, AccountId> {
     /// When action occured.
-    moderated_at: BlockchainTimestamp<BlockNumber, Moment>,
+    pub moderated_at: BlockchainTimestamp<BlockNumber, Moment>,
 
     /// Account forum sudo which acted.
-    moderator_id: AccountId,
+    pub moderator_id: AccountId,
 
     /// Moderation rationale
-    rationale: Vec<u8>,
+    pub rationale: Vec<u8>,
 }
 
 /// Represents a revision of the text of a Post
@@ -354,10 +352,10 @@ pub struct ModerationAction<BlockNumber, Moment, AccountId> {
 #[derive(Encode, Decode, Default, Clone, PartialEq, Eq)]
 pub struct PostTextChange<BlockNumber, Moment> {
     /// When this expiration occured
-    expired_at: BlockchainTimestamp<BlockNumber, Moment>,
+    pub expired_at: BlockchainTimestamp<BlockNumber, Moment>,
 
     /// Text that expired
-    text: Vec<u8>,
+    pub text: Vec<u8>,
 }
 
 /// Represents a post identifier
@@ -368,32 +366,32 @@ pub type PostId = u64;
 #[derive(Encode, Decode, Default, Clone, PartialEq, Eq)]
 pub struct Post<BlockNumber, Moment, AccountId> {
     /// Post identifier
-    id: PostId,
+    pub id: PostId,
 
     /// Id of thread to which this post corresponds.
-    thread_id: ThreadId,
+    pub thread_id: ThreadId,
 
     /// The post number of this post in its thread, i.e. total number of posts added (including this)
     /// to a thread when it was added.
     /// Is needed to give light clients assurance about getting all posts in a given range,
     // `created_at` is not sufficient.
     /// Starts at 1 for first post in thread.
-    nr_in_thread: u32,
+    pub nr_in_thread: u32,
 
     /// Current text of post
-    current_text: Vec<u8>,
+    pub current_text: Vec<u8>,
 
     /// Possible moderation of this post
-    moderation: Option<ModerationAction<BlockNumber, Moment, AccountId>>,
+    pub moderation: Option<ModerationAction<BlockNumber, Moment, AccountId>>,
 
     /// Edits of post ordered chronologically by edit time.
-    text_change_history: Vec<PostTextChange<BlockNumber, Moment>>,
+    pub text_change_history: Vec<PostTextChange<BlockNumber, Moment>>,
 
     /// When post was submitted.
-    created_at: BlockchainTimestamp<BlockNumber, Moment>,
+    pub created_at: BlockchainTimestamp<BlockNumber, Moment>,
 
     /// Author of post.
-    author_id: AccountId,
+    pub author_id: AccountId,
 }
 
 /// Represents a thread identifier
@@ -404,23 +402,23 @@ pub type ThreadId = u64;
 #[derive(Encode, Decode, Default, Clone, PartialEq, Eq)]
 pub struct Thread<BlockNumber, Moment, AccountId> {
     /// Thread identifier
-    id: ThreadId,
+    pub id: ThreadId,
 
     /// Title
-    title: Vec<u8>,
+    pub title: Vec<u8>,
 
     /// Category in which this thread lives
-    category_id: CategoryId,
+    pub category_id: CategoryId,
 
     /// The thread number of this thread in its category, i.e. total number of thread added (including this)
     /// to a category when it was added.
     /// Is needed to give light clients assurance about getting all threads in a given range,
     /// `created_at` is not sufficient.
     /// Starts at 1 for first thread in category.
-    nr_in_category: u32,
+    pub nr_in_category: u32,
 
     /// Possible moderation of this thread
-    moderation: Option<ModerationAction<BlockNumber, Moment, AccountId>>,
+    pub moderation: Option<ModerationAction<BlockNumber, Moment, AccountId>>,
 
     /// Number of unmoderated and moderated posts in this thread.
     /// The sum of these two only increases, and former is incremented
@@ -432,14 +430,14 @@ pub struct Thread<BlockNumber, Moment, AccountId> {
     ///
     /// These values are vital for light clients, in order to validate that they are
     /// not being censored from posts in a thread.
-    num_unmoderated_posts: u32,
-    num_moderated_posts: u32,
+    pub num_unmoderated_posts: u32,
+    pub num_moderated_posts: u32,
 
     /// When thread was established.
-    created_at: BlockchainTimestamp<BlockNumber, Moment>,
+    pub created_at: BlockchainTimestamp<BlockNumber, Moment>,
 
     /// Author of post.
-    author_id: AccountId,
+    pub author_id: AccountId,
 }
 
 impl<BlockNumber, Moment, AccountId> Thread<BlockNumber, Moment, AccountId> {
@@ -456,11 +454,11 @@ pub type CategoryId = u64;
 #[derive(Encode, Decode, Default, Clone, PartialEq, Eq)]
 pub struct ChildPositionInParentCategory {
     /// Id of parent category
-    parent_id: CategoryId,
+    pub parent_id: CategoryId,
 
     /// Nr of the child in the parent
     /// Starts at 1
-    child_nr_in_parent_category: u32,
+    pub child_nr_in_parent_category: u32,
 }
 
 /// Represents a category
@@ -468,22 +466,22 @@ pub struct ChildPositionInParentCategory {
 #[derive(Encode, Decode, Default, Clone, PartialEq, Eq)]
 pub struct Category<BlockNumber, Moment, AccountId> {
     /// Category identifier
-    id: CategoryId,
+    pub id: CategoryId,
 
     /// Title
-    title: Vec<u8>,
+    pub title: Vec<u8>,
 
     /// Description
-    description: Vec<u8>,
+    pub description: Vec<u8>,
 
     /// When category was established.
-    created_at: BlockchainTimestamp<BlockNumber, Moment>,
+    pub created_at: BlockchainTimestamp<BlockNumber, Moment>,
 
     /// Whether category is deleted.
-    deleted: bool,
+    pub deleted: bool,
 
     /// Whether category is archived.
-    archived: bool,
+    pub archived: bool,
 
     /// Number of subcategories (deleted, archived or neither),
     /// unmoderated threads and moderated threads, _directly_ in this category.
@@ -499,15 +497,15 @@ pub struct Category<BlockNumber, Moment, AccountId> {
     ///
     /// These values are vital for light clients, in order to validate that they are
     /// not being censored from subcategories or threads in a category.
-    num_direct_subcategories: u32,
-    num_direct_unmoderated_threads: u32,
-    num_direct_moderated_threads: u32,
+    pub num_direct_subcategories: u32,
+    pub num_direct_unmoderated_threads: u32,
+    pub num_direct_moderated_threads: u32,
 
     /// Position as child in parent, if present, otherwise this category is a root category
-    position_in_parent_category: Option<ChildPositionInParentCategory>,
+    pub position_in_parent_category: Option<ChildPositionInParentCategory>,
 
     /// Account of the moderator which created category.
-    moderator_id: AccountId,
+    pub moderator_id: AccountId,
 }
 
 impl<BlockNumber, Moment, AccountId> Category<BlockNumber, Moment, AccountId> {
